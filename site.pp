62b909f526a67d34aabf666e9c9c65d29a5fec23
hasebe <hasebe.jun@lab.ntt.co.jp>
2015-03-03 14:33:19 +0900

    追記

--- site.pp
+++ site.pp
@@ -3,4 +3,11 @@ node default {
     ensure  => present,
     content => 'This is a test file',
   }
+
+  file {'testfile':
+    path    => '/tmp/testfile',
+    ensure  => present,
+    mode    => 0640,
+    content => "I'm a test file frin Learning Puppet.",
+  }
 }
